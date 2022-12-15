95408b91a92fab8a19c5aec2b80add060190c4cf
hoxas <hoxas@live.com>
2022-12-14 23:26:09 -0300

    Update app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const workerRoutes = require("./routes/workerRoutes");
+const companyRoutes = require("./routes/companyRoutes");
+const opportunityRoutes = require("./routes/opportunityRoutes");
 const app = express();
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Vagas API",
+      version: "1.0.0",
+      description: "API para buscar vaga e/ou trabalhador",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(express.json());
 app.use(cors());
@@ -11,5 +36,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/workers", workerRoutes);
+app.use("/companies", companyRoutes);
+app.use("/jobs", opportunityRoutes);
 
 module.exports = app;
