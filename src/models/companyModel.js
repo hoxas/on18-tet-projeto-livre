a717f453b469883f4f8e6b6d0091968478b72f6f
hoxas <hoxas@live.com>
2022-12-14 23:26:02 -0300

    Update models

--- src/models/companyModel.js
+++ src/models/companyModel.js
@@ -9,7 +9,23 @@ const opportunitySchema = new Schema({
 
 const companySchema = new Schema({
   name: { type: String, required: true },
-  cnpj: { type: String, required: true },
+  cnpj: {
+    type: String,
+    required: true,
+    validate: {
+      validator: async function (cnpj) {
+        const company = await Company.findOne({ cnpj });
+        if (company) {
+          if (this.id === company.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: (props) => "O CNPJ já esta cadastrado em nosso sistema",
+    },
+  },
   location: { type: String, required: true },
   opportunities: { type: [opportunitySchema] },
 });
--- src/models/workerModel.js
+++ src/models/workerModel.js
@@ -13,7 +13,7 @@ const educationSchema = new Schema({
   school: { type: String, required: true },
   degree: { type: String, required: true },
   start_date: { type: Date, required: true },
-  end_date: { type: Date },
+  end_date: { type: Date, required: true },
   description: { type: String },
 });
 
@@ -25,6 +25,23 @@ const skillSchema = new Schema({
 const workerSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: async function (email) {
+        const user = await Worker.findOne({ email });
+        if (user) {
+          if (this.id === user.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: (props) => "O endereço de email já esta em uso.",
+    },
+  },
   location: { type: String, required: true },
   birth_date: { type: Date, required: true },
   experience: { type: [experienceSchema] },
